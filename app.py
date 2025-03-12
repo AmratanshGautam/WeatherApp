2b9131f79d08430b073f1be3dee0690c87d05eda
Amratansh Gautam <amratanshgautam@gmail.com>
2025-03-12 21:56:36 +0530

    Debugging...

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def index():
     return render_template("index.html", weather=weather_data, error=error_message) 
 if __name__ == "__main__": 
     port = int(os.environ.get("PORT", 10000)) # Use Render's port 
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
