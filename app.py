9cf45be37867c4c7449bf070a4ad0d63cd4b51e3
Amratansh Gautam <amratanshgautam@gmail.com>
2025-03-12 21:15:50 +0530

    updated port for render deployment

--- app.py
+++ app.py
@@ -30,5 +30,7 @@ def get_weather():
     
     return jsonify(weather_data)
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
